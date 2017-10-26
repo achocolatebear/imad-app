74dad7b8295eddf888b252854d13b9dbe210a384
achocolatebear <harshamadhavan@gmail.com>
2017-10-26 21:14:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,11 @@ console.log('Sup, Biyatch!');
 var element = document.getElementById('cent');
 
 element.innerHTML = 'New Wave';
+
+ // select the image
+ var img = document.getElementById('madi');
+ 
+ img.onclick = function(){
+     img.style.marginLeft = '100'px
+ };
+ 
