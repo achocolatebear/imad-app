ae8860aea303c2fed43dddd8b24ae276ed2f6974
achocolatebear <harshamadhavan@gmail.com>
2017-10-26 19:38:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,11 +63,11 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.send (createTemplate(articleOne));
+  res.sendFile(path.join(__dirname, 'ui','index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.send (createTemplate(articleOne));
 });
 
 app.get('/ui/madi.png', function (req, res) {
