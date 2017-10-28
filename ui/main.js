f6f9d3df2936a03dff2287ad546663e9d5ff7338
achocolatebear <harshamadhavan@gmail.com>
2017-10-28 13:55:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,12 @@
 // Select our element
-
 var button = document.getElementById("button");
 
 button.onclick = function (){
     
 };
+
+counter = counter + 1 
+
+var span = document.getElementById('count'){
+    span.innerHTML = counter.toString();
+}
