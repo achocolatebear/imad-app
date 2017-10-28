ec4ee860583ff7d82fa4fce7db65c701dc371c65
achocolatebear <harshamadhavan@gmail.com>
2017-10-28 13:41:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,11 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','main.js'));
 });
 
-
+var counter = 0;
+app.get('/counter', function (req,res){
+    counter = counter +1;
+    res.send =counter.toString();
+});
 
 
 app.get ('/a-1', function(req,res){
